710a445fd535005443ce0055172270efca4eb3ae
Koos85 <koosjoosten@gmail.com>
2025-11-14 14:05:55 +0100

    libprobe v2

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from libprobe.probe import Probe
-from lib.check.brocade import check_brocade
-from lib.check.fcfxport import check_fcfxport
+from lib.check.brocade import CheckBrocade
+from lib.check.fcfxport import CheckFcFxPort
 from lib.version import __version__ as version
 
 
 if __name__ == '__main__':
-    checks = {
-        'brocade': check_brocade,
-        'fcfxport': check_fcfxport,
-    }
+    checks = (
+        CheckBrocade,
+        CheckFcFxPort,
+    )
 
     probe = Probe("brocade", version, checks)
 
